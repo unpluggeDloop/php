beeda7c8ba344a384b84ac7fb6b4376174fb89a5
unpluggeDloop <unplugged.loop.mail@gmail.com>
2019-10-20 08:45:25 -0700

    Create array-count-backwards.php

---
+++ array-count-backwards.php
@@ -0,0 +1,13 @@
+<?php
+//count elements of an array array_a backwards and add it to new array array_b
+$array_a = [1,2,3];
+$array_b= [];
+
+for($i=count($array_a)-1; $i>=0; $i--){
+  $array_b[]=$array_a[$i];
+}
+
+
+echo "<pre>";
+print_r($array_b);
+echo "</pre>";
